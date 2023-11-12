78a58f16596361ec54059768d4f01bc3e657f547
Malakhi Hopkins <m.charles.hopkins@gmail.com>
2023-11-12 17:11:55 -0500

    update stuff and add ublox to launch

--- launch/robot_launch.sh
+++ launch/robot_launch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#mrsl_perch
-#export ROS_IP=192.168.129.207
-# Set ROS_MASTER_URI
-#export ROS_MASTER_URI=http://192.168.129.249:11311
+my_ip=$(hostname -I | awk '{print $1}')
 
-#scalar1
-export ROS_IP=192.168.1.101
-# Set ROS_MASTER_URI
-export ROS_MASTER_URI=http://192.168.1.100:11311
+if [[ "$my_ip" == "192.168.1.101" ]]; then
+	# Set ROS_MASTER_URI
+	export ROS_IP=192.168.1.101
+	export ROS_MASTER_URI=http://192.168.1.100:11311
+elif [[ "$my_ip" == "192.168.129.207" ]]; then
+	# Set ROS_MASTER_URI
+	export ROS_IP=192.168.129.207
+	export ROS_MASTER_URI=http://192.168.129.249:11311
+fi
 
 #mrsl_mast
 #export ROS_IP=192.168.8.56
@@ -20,10 +22,16 @@ export ROS_MASTER_URI=http://192.168.1.100:11311
 # Set ROS_MASTER_URI
 #export ROS_MASTER_URI=http://192.168.1.154:11311
 
-
-
 # Launch the roslaunch file
-roslaunch clod_pkg clod.launch &
+roslaunch clod_pkg clod_only.launch &
+#roslaunch pointgrey_camera_driver camera.launch &
+roslaunch spinnaker_camera_driver camera.launch &
 roslaunch realsense2_camera rs_t265.launch & 
+roslaunch ublox_gps ublox_device_beast.launch param_file_name:=alice param_file_dir:=/home/beast/catkin_ws/src/ublox/ublox_gps/config &
+roslaunch ntrip_client ntrip_client.launch 
+
+
+#roslaunch clod_pkg clod.launch &
+
 
 wait
