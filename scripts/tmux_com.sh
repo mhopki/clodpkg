1edf3075fc5e69be09f9d64ec6ef5f5f64e978c5
mhopki <m.charles.hopkins@gmail.com>
2024-10-08 07:14:32 -0400

    updates to tmux

--- scripts/tmux_com.sh
+++ scripts/tmux_com.sh
@@ -16,17 +16,21 @@ tmux send-keys -t $SESSION_NAME:0 'source ~/catkin_ws/src/clodpkg/scripts/rosip.
 tmux new-window -t $SESSION_NAME:1 -n 'joystick'
 tmux send-keys -t $SESSION_NAME:1 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; roscd clod_pkg/scripts/; ./com_launch.sh'
 
+# Window 1: First ROS launch
+tmux new-window -t $SESSION_NAME:2 -n 'joystick'
+tmux send-keys -t $SESSION_NAME:2 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; rosrun clod_pkg o_track.py'
+
 # Window 2: Second ROS launch
-tmux new-window -t $SESSION_NAME:2 -n 'waypoints'
-tmux send-keys -t $SESSION_NAME:2 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; rosrun clod_pkg waypoint_sender.py'
+tmux new-window -t $SESSION_NAME:3 -n 'waypoints'
+tmux send-keys -t $SESSION_NAME:3 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; rosrun clod_pkg waypoint_sender.py'
 
 # Window 3: Third ROS node
-tmux new-window -t $SESSION_NAME:3 -n 'path_code'
-tmux send-keys -t $SESSION_NAME:3 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; rosrun clod_pkg rrt.py'
+tmux new-window -t $SESSION_NAME:4 -n 'path_code'
+tmux send-keys -t $SESSION_NAME:4 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; rosrun clod_pkg rrt.py'
 
 # Window 4: Third ROS node
-tmux new-window -t $SESSION_NAME:4 -n 'KILL'
-tmux send-keys -t $SESSION_NAME:4 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; tmux kill-session -t beast_com_session'
+tmux new-window -t $SESSION_NAME:5 -n 'KILL'
+tmux send-keys -t $SESSION_NAME:5 'source ~/catkin_ws/src/clodpkg/scripts/rosip.sh; tmux kill-session -t beast_com_session'
 
 # Attach to the session
 tmux attach-session -t $SESSION_NAME
